1d6eacc943bc2888be80656921b5cedd41f606e9
sravankotha731 <sravan.kotha@3pillarglobal.com>
2022-03-23 17:42:03 +0530

    Format

--- cypress/integration/assignment/testCases.js
+++ cypress/integration/assignment/testCases.js
@@ -1,50 +1,50 @@
 /// <reference types="cypress" />
-describe("Automation Practice",()=>{
+describe("Automation Practice", () => {
     let locator
-    let random=Math.random()
+    let random = Math.random()
     let email = `sravan${random}@gmail.com`
-    before(()=>{
+    before(() => {
         cy.baseURL()
         //loading locators data
-        cy.fixture("locators.json").then((locatorsA4)=>{
+        cy.fixture("locators.json").then((locatorsA4) => {
             locator = locatorsA4
             return locator
         })
-    
+
     })
 
     //will create an account in automation practice website
-    it("Signup",()=>{
-        cy.createAccount(locator.createAaccount,email)
+    it("Signup", () => {
+        cy.createAccount(locator.createAaccount, email)
         cy.wait(4000)
         //filling signup form
-        cy.fillform(locator.form,locator.data) 
-        let customerName = locator.data["firstname"]+" "+locator.data["lastname"]
+        cy.fillform(locator.form, locator.data)
+        let customerName = locator.data["firstname"] + " " + locator.data["lastname"]
         //verifying signin customer
-        cy.verifySignin(locator.name,customerName)  
+        cy.verifySignin(locator.name, customerName)
     })
 
     //product will add to cart and verify the added product
-    it("AddtoCart & verifyProduct ",()=>{
+    it("AddtoCart & verifyProduct ", () => {
         //adding product to cart
         cy.AddtoCart(locator.addtoCart)
         cy.wait(4000)
         //verifying product aaded to cart
-        cy.verifyText(locator.productLoc,locator.ProductText)
+        cy.verifyText(locator.productLoc, locator.ProductText)
         //checkout
         cy.get(locator.checkout).click()
     })
 
     //will verify and complete the summary and payment process
-    it("Summary",()=>{
+    it("Summary", () => {
         //verifying text in summary page
-        cy.verifyText(locator.summary,locator.summarydata)
-        cy.get(locator.ProceedToCheckout).eq(1).click({force:true})
+        cy.verifyText(locator.summary, locator.summarydata)
+        cy.get(locator.ProceedToCheckout).eq(1).click({ force: true })
         cy.completeProcess(locator.process)
         //verifyng text in payment page
-        cy.verifyText(locator.summary,locator.summarydata)
+        cy.verifyText(locator.summary, locator.summarydata)
         cy.PaymentOption(locator.paymentOption)
         //verifying text after completing payment
-        cy.verifyText(locator.payment,locator.paymentText)
+        cy.verifyText(locator.payment, locator.paymentText)
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,53 +1,53 @@
 //will call baseURL
-Cypress.Commands.add('baseURL',()=>{
+Cypress.Commands.add('baseURL', () => {
     cy.visit("http://automationpractice.com/index.php")
 })
 
 //"createAccount" command will enter the email and click on the button.
-Cypress.Commands.add('createAccount',(locators,email)=>{
-    for(var property in locators){
-        property==="emailCreate"? cy.get(locators[property]).type(email) : cy.get(locators[property]).click()
+Cypress.Commands.add('createAccount', (locators, email) => {
+    for (var property in locators) {
+        property === "emailCreate" ? cy.get(locators[property]).type(email) : cy.get(locators[property]).click()
     }
 })
 
 //"fillform" command will fill all the field
-Cypress.Commands.add('fillform',(formclass,formdata)=>{
-    for(var property in formclass){
-        property==="state" || property==="country" ? cy.get(formclass[property]).select(formdata[property])
-                                                   : cy.get(formclass[property]).type(formdata[property])
+Cypress.Commands.add('fillform', (formclass, formdata) => {
+    for (var property in formclass) {
+        property === "state" || property === "country" ? cy.get(formclass[property]).select(formdata[property])
+            : cy.get(formclass[property]).type(formdata[property])
     }
     cy.get("#submitAccount").click()
 })
 
 //"verifySignin" command will verify the customer name
-Cypress.Commands.add('verifySignin',(nameclass,namecust)=>{
+Cypress.Commands.add('verifySignin', (nameclass, namecust) => {
     cy.get(nameclass).should('have.text', namecust)
 })
 
 //"AddtoCart" command will add product to cart
-Cypress.Commands.add('AddtoCart',(addProduct)=>{
-    for(var property in addProduct){
-        property==="tShirt"?cy.get(addProduct[property]).eq(0).click({force: true}):cy.get(addProduct[property]).click()
+Cypress.Commands.add('AddtoCart', (addProduct) => {
+    for (var property in addProduct) {
+        property === "tShirt" ? cy.get(addProduct[property]).eq(0).click({ force: true }) : cy.get(addProduct[property]).click()
     }
 })
 
 //"verifyText" command will invoke the text from object and comapre with text
-Cypress.Commands.add('verifyText',(ProductLoc,ProductText)=>{
-    for(var property in ProductText){
-        cy.get(ProductLoc[property]).invoke('text').should('include',ProductText[property])
+Cypress.Commands.add('verifyText', (ProductLoc, ProductText) => {
+    for (var property in ProductText) {
+        cy.get(ProductLoc[property]).invoke('text').should('include', ProductText[property])
     }
 })
 
 //"completeProcess" command will complete the checkout process in cart
-Cypress.Commands.add('completeProcess',(locator)=>{
-    for(var property in locator){
-        property==="checkbox"?cy.get(locator[property]).check() : cy.get(locator[property]).click()
+Cypress.Commands.add('completeProcess', (locator) => {
+    for (var property in locator) {
+        property === "checkbox" ? cy.get(locator[property]).check() : cy.get(locator[property]).click()
     }
 })
 
 //"Payment" command will click the payment method and completes the process
-Cypress.Commands.add('PaymentOption',(Paymentlocator)=>{
-    for(var property in Paymentlocator){
+Cypress.Commands.add('PaymentOption', (Paymentlocator) => {
+    for (var property in Paymentlocator) {
         cy.get(Paymentlocator[property]).click()
     }
 })
